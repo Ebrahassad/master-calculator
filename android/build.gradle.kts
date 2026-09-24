5a6afb6eb14077f0ce9f874193ab1f64bb5b6334
Ebrahassad <ebrahassadi1@gmail.com>
2026-09-24 17:32:33 +0200

    fix: restore clean and valid syntax for root android/build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,27 +1,18 @@
-
 allprojects {
-    tasks.withType<JavaCompile>().configureEach {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
-    }
-}
-
     repositories {
         google()
         mavenCentral()
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
